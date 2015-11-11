11bc8dbaaca9b4600ef359c4be125011c3698a45
dmziryanov <dmziryanov@yandex.ru>
2015-11-12 02:00:14 +0300

    init commit

---
+++ BookSample/Common/BookReaders.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using BookSample.Models;
+
+namespace BookSample.Common
+{
+    public class BookReaderFactory : IBookReaderFactory
+    {
+        public BookReader GetReader(string fileExt)
+        {
+            if (fileExt == ".xml")
+            {
+                return new XmlBookReader();
+            }
+
+            return new CsvBookReader();
+        }
+    }
+
+    public interface IBookReaderFactory
+    {
+        BookReader GetReader(string fileExt);
+    }
+
+    public abstract class BookReader
+    {
+        public abstract IEnumerable<Book> GetBooks(StreamReader stream);
+    }
+
+    public class XmlBookReader : BookReader
+    {
+        public override IEnumerable<Book> GetBooks(StreamReader stream)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(BookList));
+            return ((BookList)serializer.Deserialize(stream)).Books;
+        }
+    }
+
+    public class CsvBookReader : BookReader
+    {
+        public override IEnumerable<Book> GetBooks(StreamReader stream)
+        {
+            var csv = stream.ReadLine();
+            while (!string.IsNullOrEmpty(csv))
+            {
+                var str = csv.Split(';');
+                var book = new Book()
+                {
+                    Title = str[0],
+                    Author = str[1],
+                    Year = Convert.ToInt32(str[2])
+                };
+
+                yield return book;
+                csv = stream.ReadLine();
+            }
+        }
+    }
+}
---
+++ BookSample/Repository/IBookRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using BookSample.Models;
+
+namespace Repository
+{
+    public interface IBookRepository : IGenericRepository<Book>
+    {
+        IEnumerable<Book> GetFilteredSortedPage(BookFieldType sortBy, BookFieldType filterBy, string filterString, int pageIndex, int pageSize);
+        int GetFilteredSortedPageCount(BookFieldType filterBy, string filterString, int pageSize);
+        int GetTotalCount();
+    }
+}
